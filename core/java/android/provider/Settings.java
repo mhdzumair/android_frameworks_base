4a12b53c6b809f820d82254c55b0001aff1e7aa3
Dark Eyes <dark_eyes_@hotmail.com>
2017-09-03 21:34:04 -0400

    Two columns in dashboard
    Change-Id: Iacd8d58420cda8087188cf42902af3f840c64bed

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3789,6 +3789,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
 
         /**
+         * Enable two columns in settings
+         * @hide
+         */
+        public static final String TWO_COLUMNS = "two_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
